02296b03463a0510ffa50d555aafdc5289e3d01b
icecoder <980668011@qq.com>
2020-11-03 21:44:27 +0800

    每日一题：有效的山脉数组(优化解法)

--- src/main/java/com/icecoder/leecode/everyday/MountainArrayTest.java
+++ src/main/java/com/icecoder/leecode/everyday/MountainArrayTest.java
@@ -8,10 +8,34 @@ package com.icecoder.leecode.everyday;
 public class MountainArrayTest {
 
     public static void main(String[] args) {
-        System.out.println(new MountainArrayTest().validMountainArray(new int[]{2, 1}));
-        System.out.println(new MountainArrayTest().validMountainArray(new int[]{3, 5, 5}));
-        System.out.println(new MountainArrayTest().validMountainArray(new int[]{0, 3, 2, 1}));
-        System.out.println(new MountainArrayTest().validMountainArray(new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}));
+        System.out.println(new MountainArrayTest().validMountainArray2(new int[]{2, 1}));
+        System.out.println(new MountainArrayTest().validMountainArray2(new int[]{3, 5, 5}));
+        System.out.println(new MountainArrayTest().validMountainArray2(new int[]{0, 3, 2, 1}));
+        System.out.println(new MountainArrayTest().validMountainArray2(new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}));
+    }
+
+    public boolean validMountainArray2(int[] A) {
+        if (A.length < 3) {
+            return false;
+        }
+        int h = 0;
+        for (int i = 0; i < A.length; i++) {
+            if (A[h] < A[i]) {
+                h = i;
+            }
+        }
+        if (h == 0 || (h + 1) == A.length) {
+            return false;
+        }
+        int l = h - 1;
+        while (l >= 0 && A[l] < A[l + 1]) {
+            l--;
+        }
+        int r = h + 1;
+        while (r < A.length && A[r - 1] > A[r]) {
+            r++;
+        }
+        return (l == -1 && r == A.length);
     }
 
 
